23b6d764633ea61a934231e2d3d7752bcdc44c7b
Thibaut Mallet <tmallet@cardiweb.com>
2021-01-14 12:47:43 +0100

    Fix module exports

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var fs = require('fs'),
     path = require('path');
 
 ['server', 'client', 'Agents'].forEach(function(f) {
-  var exp = require(__dirname + '/lib/' + f),
+  var exp = require('./lib/' + f),
       keys = Object.keys(exp);
   for (var i = 0, len = keys.length; i < len; ++i)
     exports[keys[i]] = exp[keys[i]];
@@ -10,6 +10,6 @@ var fs = require('fs'),
 
 exports.auth = {};
 
-fs.readdirSync(__dirname + '/lib/auth').forEach(function(f) {
-  exports.auth[path.basename(f, '.js')] = require(__dirname + '/lib/auth/' + f);
+['None', 'UserPassword'].forEach(function(f) {
+  exports.auth[f] = require('./lib/auth/' + f);
 });
